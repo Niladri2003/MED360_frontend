b511d3c848623aeac9d95f8e2f710d154a866c20
Arnab Pal <arnab.pal.21@aot.edu.in>
2023-08-28 01:22:03 +0530

    Update HomeCardGroup.jsx

--- src/components/Common/HomeCardGroup.jsx
+++ src/components/Common/HomeCardGroup.jsx
@@ -6,7 +6,7 @@ import Card from "./card/Card"
 
 const HomeCardGroup = () => {
   return (
-    <div className="mb-20 mt-10 flex flex-row gap-6">
+    <div className="mb-20 mt-10 grid grid-cols-1 md:grid-cols-3  lg:grid-cols-4 flex-row gap-6">
       {/* Card */}
       <div className=" w-25 flex h-36 flex-col items-center justify-around rounded-md border-[1px] border-white p-4 shadow-md shadow-black hover:bg-[#3098aa] hover:shadow-lg hover:shadow-black">
         <img src={BookingIcon} className="h-10" />
