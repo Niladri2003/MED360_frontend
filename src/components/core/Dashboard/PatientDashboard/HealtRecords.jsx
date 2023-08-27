72d6773646ef2ee7a164c6d599f2e0057af6d3d0
Arnab Pal <arnab.pal.21@aot.edu.in>
2023-08-27 16:48:10 +0530

    done health card pdf

--- src/components/core/Dashboard/PatientDashboard/HealtRecords.jsx
+++ src/components/core/Dashboard/PatientDashboard/HealtRecords.jsx
@@ -3,7 +3,15 @@ import Health_card from "./Health_card";
 
 const HealtRecords = () => {
   return (
-    <div className="grid grid-cols-5">
+    <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-4 gap-x-10 lg:gap-y-6  md:gap-y-4 gap-y-4
+    ">
+      <Health_card/>
+      <Health_card/>
+      <Health_card/>
+      <Health_card/>
+      <Health_card/>
+      <Health_card/>
+      <Health_card/>
       <Health_card/>
     </div>
   )
--- src/components/core/Dashboard/PatientDashboard/Health_card.jsx
+++ src/components/core/Dashboard/PatientDashboard/Health_card.jsx
@@ -2,24 +2,42 @@ import React from 'react'
 import pdf_logo from "../../../../assets/Images/pdf_logo.png";
 
 const Health_card = () => {
-  return (
-    <div>
+    return (
+        <div className='flex gap-5 '>
 
-        <div className="py-6 border-[1px] border-white">
-        <img src={pdf_logo} alt="pdf image" className="h-24 w-20" />
+            <div className="py-6  border-[1px] bg-richblack-5 border-white rounded-lg  transform transition duration-500 hover:scale-105 p-4 flex flex-col items-center">
+                
 
-        <div >
-        <button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Purple</button>
+                    <img src={pdf_logo} alt="pdf image" className="h-[150px] w-[150px] " />
+                 
+                <div className='flex flex-col justify-center items-center '>
 
+                    <button
+                        type="button"
+                        class="px-9 py-3 bg-blue-600 rounded-md text-white outline-none focus:ring-4 shadow-lg transform active:scale-x-75 transition-transform mx-5 flex my-2"
+                    >
+                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                        </svg>
+                        <span class="ml-2">View</span>
+                    </button>
+                    <button
+                        type="button"
+                        class="px-4 py-3 bg-blue-600 rounded-md text-white outline-none focus:ring-4 shadow-lg transform active:scale-x-75 transition-transform mx-5 flex my-2"
+                    >
+                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+                        </svg>
 
-        <button type="button" class="text-white bg-gradient-to-r bg-purple-500 bg-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Purple</button>
+                        <span class="ml-2">Download</span>
+                    </button>
+
+                </div>
+            </div>
 
 
         </div>
-      </div>
-
-    </div>
-  )
+    )
 }
 
 export default Health_card
