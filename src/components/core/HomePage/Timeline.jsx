0e615b1550bb3e1ef6e92e71ce97de6911d84509
Arnab Pal <arnab.pal.21@aot.edu.in>
2023-08-28 12:08:01 +0530

    color icon added

--- src/components/core/HomePage/Timeline.jsx
+++ src/components/core/HomePage/Timeline.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import TimeLineImage from "../../../assets/Images/TimelineImage.png";
-import Logo1 from "../../../assets/TimeLineLogo/Logo1.svg";
-import Logo2 from "../../../assets/TimeLineLogo/Logo2.svg";
-import Logo3 from "../../../assets/TimeLineLogo/Logo3.svg";
-import Logo4 from "../../../assets/TimeLineLogo/Logo4.svg";
+import Logo1 from "../../../assets/Icons/browser.png";
+import Logo2 from "../../../assets/Icons/select.png";
+import Logo3 from "../../../assets/Icons/form.png";
+import Logo4 from "../../../assets/Icons/enter.png";
 
 const TimeLine = [
     {
